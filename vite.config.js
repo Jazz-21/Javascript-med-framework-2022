7b63ab4c3c4a49f225cdd127772c7cdac738d83a
Jazz-21 <93574557+Jazz-21@users.noreply.github.com>
2022-02-25 09:53:39 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,57 @@
+
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        background_color: '#fff',
+        display: 'standalone',
+        icons: [
+          {
+            purpose: 'any',
+            sizes: '192x192',
+            src: 'manifest-icon-192.maskable.png',
+            type: 'image/png'
+          },
+          {
+            purpose: 'maskable',
+            sizes: '192x192',
+            src: 'manifest-icon-192.maskable.png',
+            type: 'image/png'
+          },
+          {
+            purpose: 'any',
+            sizes: '512x512',
+            src: 'manifest-icon-512.maskable.png',
+            type: 'image/png'
+          },
+          {
+            purpose: 'maskable',
+            sizes: '512x512',
+            src: 'manifest-icon-512.maskable.png',
+            type: 'image/png'
+          }
+        ],
+        name: 'Vueapplication',
+        short_name: 'Vueapp',
+        start_url: '.',
+        theme_color: '#ffff'
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['assets/**', 'index.html', 'manifest.webmanifest'],
+        runtimeCaching: [
+          {
+            handler: 'NetworkFirst',
+            // Cache GET requests to https://avancera.app/cities/
+            urlPattern: 'https://fakestoreapi.com/products'
+          },
+        ],
+      }
+    }),
+    vue()
+  ]
+})
